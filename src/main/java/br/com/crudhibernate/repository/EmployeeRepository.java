96bbc812e07cbd1f77c3dc027070cbc5fc3dad18
Bruno Oliveira Marques <brunoliveira.marques@gmail.com>
2021-05-12 23:24:37 -0300

    Estrutura do projeto sem testes

---
+++ src/main/java/br/com/crudhibernate/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package br.com.crudhibernate.repository;
+
+import br.com.crudhibernate.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
